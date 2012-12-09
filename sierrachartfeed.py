fd318018fd73fb74a27509bcd6e2b33f57ed7c8c
slush <info@bitcoin.cz>
2012-12-09 13:06:27 +0000

    Oh, I used ancient version for initial commit. This is the latest one.

--- sierrachartfeed.py
+++ sierrachartfeed.py
@@ -3,30 +3,31 @@ Created on 14.04.2011
 
 @author: slush
 @licence: Public domain
+@version 0.5
 '''
 
 from optparse import OptionParser
 import datetime
 import time
-import asyncore
 import os
 import sys
 import urllib2
-import threading
+import socket
 
 try:
     import simplejson as json
 except ImportError:
     import json
 
-from pywsc.websocket import WebSocket
 from scid import ScidFile, ScidRecord
 
-MTGOX_WEBSOCKET_URL = 'ws://websocket.mtgox.com:80/mtgox'
 BITCOINCHARTS_TRADES_URL = 'http://bitcoincharts.com/t/trades.csv'
+BITCOINCHARTS_SOCKET = ('bitcoincharts.com', 8002)
 
-def bitcoincharts_history(symbol, from_timestamp, log=False):
-    req = urllib2.Request('%s?start=%s&end=99999999999999&symbol=%s' % (BITCOINCHARTS_TRADES_URL, from_timestamp, symbol))
+def bitcoincharts_history(symbol, from_timestamp, volume_precision, log=False):
+    url = '%s?start=%s&end=99999999999999&symbol=%s' % (BITCOINCHARTS_TRADES_URL, from_timestamp, symbol)
+    #print url
+    req = urllib2.Request(url)
     for line in urllib2.urlopen(req).read().split('\n'):
         if not line:
             continue
@@ -34,9 +35,9 @@ def bitcoincharts_history(symbol, from_timestamp, log=False):
         line = line.split(',')
         
         try:
-            timestamp, price, volume = int(line[0]), float(line[1]), int(float(line[2])*10**2)
+            timestamp, price, volume = int(line[0]), float(line[1]), int(float(line[2])*10**volume_precision)
             if log:
-                print symbol, datetime.datetime.fromtimestamp(timestamp), timestamp, price, volume/100.
+                print symbol, datetime.datetime.fromtimestamp(timestamp), timestamp, price, float(volume)/10**volume_precision
             yield ScidRecord(datetime.datetime.fromtimestamp(timestamp), price, price, price, price, volume, volume, 0, 0)
         except ValueError:
             print line
@@ -45,9 +46,10 @@ def bitcoincharts_history(symbol, from_timestamp, log=False):
         
         
 class ScidHandler(object):
-    def __init__(self, symbol, datadir, disable_history):
+    def __init__(self, symbol, datadir, disable_history, volume_precision):
         self.symbol = symbol
         self.filename = os.path.join(datadir, "%s.scid" % symbol)
+        self.volume_precision = volume_precision
         self.load()
         if not disable_history:
             try:
@@ -79,26 +81,16 @@ class ScidHandler(object):
             
         print 'Downloading historical data'
         self.scid.seek(self.scid.length)
-        for rec in bitcoincharts_history(self.symbol, from_timestamp, True):
+        for rec in bitcoincharts_history(self.symbol, from_timestamp, self.volume_precision, True):
             self.scid.write(rec.to_struct())
         self.scid.fp.flush()
          
-    def ticker_update(self, msg):
-        '''mtgox websocket ticker'''
-
-        data = json.loads(msg)
-        if data['channel'] != 'dbf1dee9-4f2e-4a08-8cb7-748919a71b21':
-            return
-    
-        #print data
-        if data.get('origin') != 'broadcast':
-            return
-    
-        price = float(data['trade']['price'])
-        volume = int(float(data['trade']['amount'])*10**2)
-        date = datetime.datetime.fromtimestamp(float(data['trade']['date']))
+    def ticker_update(self, data):        
+        price = float(data['price'])
+        volume = int(float(data['volume'])*10**self.volume_precision)
+        date = datetime.datetime.fromtimestamp(float(data['timestamp']))
         
-        print self.symbol, date, price, volume/100.
+        print self.symbol, date, price, float(volume)/10**self.volume_precision
         
         # Datetime, Open, High, Low, Close, NumTrades, TotalVolume, BidVolume, AskVolume):
         try:
@@ -108,111 +100,98 @@ class ScidHandler(object):
         except Exception as e:
             print str(e)
   
-class PollThread(threading.Thread):
-    def __init__(self, askrate, scids, symbols):
-        self.askrate = askrate
-        self.symbols = symbols
-        self.scids = scids
-        self.stop = False
-        super(PollThread, self).__init__()
-        
-    def run(self):
-        from_timestamp = {}
-        for symbol in self.symbols:
-            scid = self.scids[symbol].scid
-            if not scid.length:
-                from_timestamp[symbol] = 0
-            else:
-                scid.seek(scid.length-1)
-                rec = ScidRecord.from_struct(scid.readOne())
-                from_timestamp[symbol] = int(time.mktime(rec.DateTime.timetuple()))+1
-            scid.seek(scid.length)
-
-        while not self.stop:
-            for symbol in self.symbols:
-                scid = self.scids[symbol].scid
-                print "Updating", symbol, from_timestamp[symbol]
-                
-                #scid.seek(scid.length)
-                change = False
-                try:
-                    for rec in bitcoincharts_history(symbol, from_timestamp[symbol], log=True if from_timestamp[symbol]>0 else False):
-                        scid.write(rec.to_struct())
-                        change = True
-                                            
-                    if change:
-                        from_timestamp[symbol] = max(from_timestamp[symbol], int(time.mktime(rec.DateTime.timetuple()))+1)
-                        scid.fp.flush()
-
-                except Exception as e:
-                    print str(e)
-                    
-            for i in range(int(self.askrate)):
-                if self.stop:
-                    break
-                time.sleep(1)
+def linesplit(sock):
+    buffer = ''
+    while True:
+        try:
+            r = sock.recv(1024)
+            if r == '':
+                raise Exception("Socket failed")
             
-def on_open():
-    print "Connection to mtgox open"
-    
-def on_error(e):
-    print "Some error occured on mtgox connection:", str(e)
-    
-def on_close():
-    print "Connection closed by mtgox server"
-    
+            buffer = ''.join([buffer, r])
+        except Exception as e:
+            if str(e) != 'timed out': # Yes, there's not a better way...
+                raise
+
+        while "\n" in buffer:
+            (line, buffer) = buffer.split("\n", 1)
+            yield line
+
+class ScidLoader(dict):
+    def __init__(self, datadir, disable_history, volume_precision):
+        super(ScidLoader, self).__init__() # Don't use any template dict
+        
+        self.datadir = datadir
+        self.disable_history = disable_history
+        self.volume_precision = volume_precision
+        
+    def __getitem__(self, symbol):
+        try:
+            return dict.__getitem__(self, symbol)
+        except KeyError:
+            handler = ScidHandler(symbol, self.datadir, self.disable_history, self.volume_precision)
+            self[symbol] = handler
+            return handler
+         
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-d", "--datadir", dest="datadir", default='c:/SierraChart/data/',
                   help="Data directory of SierraChart software")
-    parser.add_option("-w", "--disable-websocket", action='store_true', default=False,
-                  help="Disable websocket connection to mtgox, use polling instead")
     parser.add_option("-y", "--disable-history", action='store_true', default=False,
                   help="Disable downloads from bitcoincharts.com")
-    parser.add_option("-s", "--symbols", dest='symbols', default='mtgoxUSD',
-                  help="Charts to watch, comma separated")
-    parser.add_option("-a", "--askrate", dest='askrate', default=10,
-                  help="How often poll chart data (not used for Mtgox)")
+    parser.add_option("-p", "--volume-precision", default=2, dest="precision", type="int",
+                  help="Change decimal precision for market volume.")
+    parser.add_option("-s", "--symbols", dest='symbols', default='mtgoxUSD,*',
+                  help="Charts to watch, comma separated. Use * for streaming all markets.")
 
     (options, args) = parser.parse_args()
 
+    if options.precision < 0 or options.precision > 8:
+        print "Precision must be between 0 and 8"
+        sys.exit()
+
     # Symbols to watch    
     symbols = options.symbols.split(',')
-
-    scids = {}
-    for symbol in symbols:
-        scids[symbol] = ScidHandler(symbol, options.datadir, options.disable_history)
-        
-        if not options.disable_websocket and symbol == 'mtgoxUSD':
-            socket = WebSocket(MTGOX_WEBSOCKET_URL)
-            socket.setEventHandlers(on_open, scids[symbol].ticker_update, on_close)  
-            socket.connect()
-
-    if not options.disable_websocket and symbol == 'mtgoxUSD':
-        # Don't poll mtgox chart
-        symbols.remove(symbol)
-        
-    t = PollThread(max(5, options.askrate), scids, symbols)
-    t.daemon = True
-    t.start()
-    
-    try:
-        if not options.disable_websocket and 'mtgoxUSD' in options.symbols:
-            asyncore.loop()
+    scids = ScidLoader(options.datadir, options.disable_history, options.precision)
             
-        while True:
-            time.sleep(5)
-    except KeyboardInterrupt:
-        #socket.close()
-        if not options.disable_websocket and 'mtgoxUSD' in options.symbols:
-            print "Connection to mtgox closed"
+    for s in symbols:
+        if s != '*':
+            scids[s]
         
-    print "Stopping polling thread"
-    t.stop = True
-    t.join()
+    while True:
+        try:
+            print "Opening streaming socket..."
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(1)
+            s.connect(BITCOINCHARTS_SOCKET)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+            s.send("{\"action\": \"subscribe\", \"channel\": \"tick\"}\n")
+            
+            for line in linesplit(s):
+                rec = json.loads(line)
+                if not rec['channel'].startswith('tick.'):
+                    # Not a tick data
+                    continue
+                
+                symbol = rec['channel'].rsplit('.')[1]
+                if symbol not in symbols and '*' not in symbols:
+                    # Filtering out symbols which user don't want to store
+                    # If '*' is in symbols, don't filter anything
+                    continue
+                
+                #print "%s: %s" % (symbol, rec['payload'])
+                scids[symbol].ticker_update(rec['payload'])
+
+        except KeyboardInterrupt:
+            print "Ctrl+C detected..."
+            break
+        except Exception as e:
+            print "%s, retrying..." % str(e)
+            time.sleep(5)
+            continue
+        finally:
+            print "Stopping streaming socket..."
+            s.close()
     
     for scid in scids.values():
         scid.scid.close()
-
-    # Terminate asyncore internals        
-    sys.exit()
